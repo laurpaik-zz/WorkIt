9f5e4f8d9b18415d6ec6dd846dfdb0423e54edaa
laurpaik <laurpaik@gmail.com>
2017-02-17 19:26:55 -0500

    Create scripts folder for workouts
    -copy auth into it
    -tomorrow you need to change/cater it to your workout api stuff

---
+++ assets/scripts/workout/api.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const config = require('../config.js');
+const store = require('../store.js');
+
+const create = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/logs',
+    method: 'POST',
+    headers: {
+      Authorization: `Token token=${store.user.token}`,
+    },
+    data,
+  });
+};
+
+const index = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/games',
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${store.user.token}`,
+    },
+  });
+};
+
+const show = function (id) {
+  return $.ajax({
+    url: config.apiOrigin + '/games/' + id,
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${store.user.token}`,
+    },
+  });
+};
+
+const update = function (id, gameIndex, player, gameOver) {
+  return $.ajax({
+    url: config.apiOrigin + '/games/' + id,
+    method: 'PATCH',
+    headers: {
+      Authorization: `Token token=${store.user.token}`,
+    },
+    data: {
+      game: {
+        cell: {
+          index: gameIndex,
+          value: player,
+        },
+        over: gameOver,
+      },
+    },
+  });
+};
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  game,
+};
---
+++ assets/scripts/workout/ui.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const success = (data) => {
+  if (data) {
+  }
+};
+
+const signInSuccess = (data) => {
+  if (data) {
+
+  }
+  $('#enter-password').val('sign in');
+  $('#sign-up-button').val('sign up');
+  $('.inputs').val('');
+  $('#sign-in').hide();
+  $('#sign-up').hide();
+  $('#change-password').show();
+  $('#sign-out').show();
+  $('#begin').show();
+  $('#game-search').show();
+  $('#search-result').show();
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+const signInFailure = (error) => {
+  console.error(error);
+  $('#enter-password').val('please try again.');
+};
+
+const signUpSuccess = (data) => {
+  if (data) {
+  }
+  $('#sign-up').hide();
+};
+
+const emailFailure = (error) => {
+  console.error(error);
+  $('#sign-up-button').val('try a different email or check your password');
+};
+
+
+
+module.exports = {
+  failure,
+  success,
+  signInFailure,
+  signInSuccess,
+  signUpSuccess,
+  emailFailure,
+};
