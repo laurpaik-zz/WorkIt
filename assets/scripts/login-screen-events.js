59618950096a6369caa3a13f086bacea9fa2f9e6
laurpaik <laurpaik@gmail.com>
2017-02-20 21:42:07 -0500

    Remove tic tac toe click handlers

--- assets/scripts/login-screen-events.js
+++ assets/scripts/login-screen-events.js
@@ -4,11 +4,7 @@ const addHandlers = () => {
 //initial screen
   $('#sign-out').hide();
   $('#change-password').hide();
-  $('#board').hide();
-  $('.banner').hide();
-  $('#game-search').hide();
-  $('#reset').hide();
-  $('#begin').hide();
+
 //on signing out, goes back to original sign in page
   $('#sign-out').on('submit', () => {
     $('#sign-in').show();
