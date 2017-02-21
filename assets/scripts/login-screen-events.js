6ff5641c958c63809025c641561cf7010a8b90a7
laurpaik <laurpaik@gmail.com>
2017-02-21 14:36:08 -0500

    Include initial screen and sign out screen

--- assets/scripts/login-screen-events.js
+++ assets/scripts/login-screen-events.js
@@ -4,6 +4,12 @@ const addHandlers = () => {
 //initial screen
   $('#sign-out').hide();
   $('#change-password').hide();
+  $('#post-athlete').hide();
+  // $('#log-search').hide();
+  $('#post-log').hide();
+  $('#edit-log').hide();
+  $('#log-destroy').hide();
+  $('#edit-athlete').hide();
 
 //on signing out, goes back to original sign in page
   $('#sign-out').on('submit', () => {
@@ -11,6 +17,14 @@ const addHandlers = () => {
     $('#sign-up').show();
     $('#sign-out').hide();
     $('#change-password').hide();
+    $('#post-athlete').hide();
+    // $('#log-search').hide();
+    $('#post-log').hide();
+    $('#edit-log').hide();
+    $('#log-destroy').hide();
+    $('#edit-athlete').hide();
+    $('#view-logs').css('display', 'none');
+    $('.table').html();
   });
 };
 
