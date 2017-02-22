ca9864d35801260a5d641ea6473176278dbd29db
laurpaik <laurpaik@gmail.com>
2017-02-22 00:23:32 -0500

    Comment out unnecessary fluff, add handlers

--- assets/scripts/workout/events.js
+++ assets/scripts/workout/events.js
@@ -2,43 +2,43 @@
 
 const api = require('./api.js');
 const ui = require('./ui.js');
-const store = require('../store');
+// const store = require('../store');
 
-const getFormFields = require('../../../lib/get-form-fields');
+// const getFormFields = require('../../../lib/get-form-fields');
 
-const onCreateWorkout = function (event) {
-  event.preventDefault();
-
-  let data = getFormFields(event.target);
-  api.create(data)
-    .then((response) => {
-      store.workout = response.workout;
-    })
-    .then(ui.onPostSuccess)
-    .catch(ui.onError);
-};
+// const onCreateWorkout = function (event) {
+//   event.preventDefault();
+//
+//   let data = getFormFields(event.target);
+//   api.create(data)
+//     .then((response) => {
+//       store.workout = response.workout;
+//     })
+//     .then(ui.onPostSuccess)
+//     .catch(ui.onError);
+// };
 
 const onGetWorkouts = function (event) {
   event.preventDefault();
-  let data = getFormFields(event.target);
-
-  if (data.workout.id.length === 0) {
+  // let data = getFormFields(event.target);
+  //
+  // if (data.workout.id.length === 0) {
     api.index()
     .then(ui.onIndexSuccess)
     .catch(ui.onError);
-  } else {
-    api.show(data.workout.id)
-    .then(ui.onGetSuccess)
-    .catch(ui.onError);
-  }
+  // } else {
+  //   api.show(data.workout.id)
+  //   .then(ui.onGetSuccess)
+  //   .catch(ui.onError);
+  // }
 };
 
-// const addHandlers = () => {
-//   $('#search-submit').on('submit', onGetGames);
-// };
+const addHandlers = () => {
+  $('#sign-in').on('submit', onGetWorkouts);
+};
 
 module.exports = {
-  onCreateWorkout,
+  // onCreateWorkout,
   onGetWorkouts,
-  // addHandlers,
+  addHandlers,
 };
