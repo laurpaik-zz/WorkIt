dea60b6a245688f9558f0d61a91dbad0f859bbfb
laurpaik <laurpaik@gmail.com>
2017-02-22 11:50:55 -0500

    Delete commented out stuff

--- assets/scripts/workout/events.js
+++ assets/scripts/workout/events.js
@@ -2,43 +2,13 @@
 
 const api = require('./api.js');
 const ui = require('./ui.js');
-// const store = require('../store');
-
-// const getFormFields = require('../../../lib/get-form-fields');
-
-// const onCreateWorkout = function (event) {
-//   event.preventDefault();
-//
-//   let data = getFormFields(event.target);
-//   api.create(data)
-//     .then((response) => {
-//       store.workout = response.workout;
-//     })
-//     .then(ui.onPostSuccess)
-//     .catch(ui.onError);
-// };
 
 const onGetWorkouts = function () {
-  // event.preventDefault();
-  // let data = getFormFields(event.target);
-  //
-  // if (data.workout.id.length === 0) {
     api.index()
     .then(ui.onIndexSuccess)
     .catch(ui.onError);
-  // } else {
-  //   api.show(data.workout.id)
-  //   .then(ui.onGetSuccess)
-  //   .catch(ui.onError);
-  // }
 };
 
-// const addHandlers = () => {
-//   $('#sign-in').on('submit', onGetWorkouts);
-// };
-
 module.exports = {
-  // onCreateWorkout,
   onGetWorkouts,
-  // addHandlers,
 };
