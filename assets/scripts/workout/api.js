629fae5c65da33299391b755aa225dce1c72fb1d
laurpaik <laurpaik@gmail.com>
2017-02-22 12:03:14 -0500

    Remove everything except index
    -I don't need it yet
    -look at forLater branch if you want it back

--- assets/scripts/workout/api.js
+++ assets/scripts/workout/api.js
@@ -1,42 +1,14 @@
 'use strict';
 
 const config = require('../config.js');
-// const store = require('../store.js');
-
-// const create = function (data) {
-//   return $.ajax({
-//     url: config.apiOrigin + '/workouts',
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Token token=${store.user.token}`,
-//     },
-//     data,
-//   });
-// };
 
 const index = function () {
   return $.ajax({
     url: config.apiOrigin + '/workouts',
     method: 'GET',
-    // headers: {
-    //   Authorization: `Token token=${store.user.token}`,
-    // },
   });
 };
 
-// const show = function (id) {
-//   return $.ajax({
-//     url: config.apiOrigin + '/workouts/' + id,
-//     method: 'GET',
-//     headers: {
-//       Authorization: `Token token=${store.user.token}`,
-//     },
-//   });
-// };
-
 module.exports = {
   index,
-  // show,
-  // create,
-  // update,
 };
--- assets/scripts/workout/ui.js
+++ assets/scripts/workout/ui.js
@@ -2,14 +2,6 @@
 
 const indexWorkoutsHandlebars = require('../templates/helpers/index-workouts.handlebars');
 
-const onSuccess = function (data) {
-  if (data.workout) {
-    console.log(data.workout);
-  } else {
-    console.log(data.workouts);
-  }
-};
-
 const onIndexSuccess = function (data) {
   $('#view-workouts').css('display', 'initial');
 
@@ -17,29 +9,11 @@ const onIndexSuccess = function (data) {
   $('#workit').html(indexWorkoutsHtml);
 };
 
-const onGetSuccess = function (data) {
-  if (data.workout) {
-    console.log(data.workout);
-  }
-};
-
 const onError = function (response) {
   console.error(response);
 };
 
-const onPostSuccess = function (data) {
-  console.log(data);
-};
-
-const onPatchSuccess = function (data) {
-  console.log(data.workout);
-};
-
 module.exports = {
-  onSuccess,
   onError,
-  onPostSuccess,
-  onPatchSuccess,
-  onGetSuccess,
   onIndexSuccess,
 };
