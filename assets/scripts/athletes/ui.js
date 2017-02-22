8af54df3ef62a474563e6279180bb33bd42c80b3
laurpaik <laurpaik@gmail.com>
2017-02-22 15:59:29 -0500

    Add update athlete success message

--- assets/scripts/athletes/ui.js
+++ assets/scripts/athletes/ui.js
@@ -2,10 +2,18 @@
 
 const onError = function (response) {
   console.error(response);
+  $('#update-athlete-warning').text('Something went wrong. Did you fill in the blanks?');
+  $('#create-log-warning').text('');
+  $('#delete-log-warning').text('');
+  $('#update-log-warning').text('');
 };
 
 const onUpdateSuccess = function (data) {
   console.log(data);
+  $('#update-athlete-warning').text('Athlete profile updated!');
+  $('#create-log-warning').text('');
+  $('#delete-log-warning').text('');
+  $('#update-log-warning').text('');
 };
 
 module.exports = {
