b03634df14a3f5b6c202dca74515bc3a33a3c476
laurpaik <laurpaik@gmail.com>
2017-02-22 00:22:00 -0500

    Comment out unnecessary fluff

--- assets/scripts/workout/api.js
+++ assets/scripts/workout/api.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = require('../config.js');
-const store = require('../store.js');
+// const store = require('../store.js');
 
 // const create = function (data) {
 //   return $.ajax({
@@ -18,25 +18,25 @@ const index = function () {
   return $.ajax({
     url: config.apiOrigin + '/workouts',
     method: 'GET',
-    headers: {
-      Authorization: `Token token=${store.user.token}`,
-    },
+    // headers: {
+    //   Authorization: `Token token=${store.user.token}`,
+    // },
   });
 };
 
-const show = function (id) {
-  return $.ajax({
-    url: config.apiOrigin + '/workouts/' + id,
-    method: 'GET',
-    headers: {
-      Authorization: `Token token=${store.user.token}`,
-    },
-  });
-};
+// const show = function (id) {
+//   return $.ajax({
+//     url: config.apiOrigin + '/workouts/' + id,
+//     method: 'GET',
+//     headers: {
+//       Authorization: `Token token=${store.user.token}`,
+//     },
+//   });
+// };
 
 module.exports = {
   index,
-  show,
+  // show,
   // create,
   // update,
 };
