39069d0f66a6c538e51cf6fa71bd942131c271e0
laurpaik <laurpaik@gmail.com>
2017-02-23 00:08:29 -0500

    Set tabs back to collapse onSignOut

--- assets/scripts/login-screen-events.js
+++ assets/scripts/login-screen-events.js
@@ -38,6 +38,7 @@ const addHandlers = () => {
     $('#changepw').val('Change');
     $('#changePwSuccess').text('');
     $('.warning').text('');
+    $('.collapsed').attr('data-toggle').modal('collapse');
   });
 };
 
